a2edf7e5213a5d5b61185d70ed72cc07415efba8
matheusOliv23 <matheushenri1996@gmail.com>
2021-09-07 07:25:33 -0300

    componente do produto

--- src/components/Promotion/Card/Card.js
+++ src/components/Promotion/Card/Card.js
@@ -1,7 +1,21 @@
 import React from 'react'
 
-const PromoCard = () => {
+const PromotionCard = ({promotion}) => (
+    <div>
+        <img src={promotion.imageUrl} alt="imagem do computador" />
+        <div>
+            <h1>{promotion.title}</h1>
+            <span>R$ {promotion.price}</span>
+            <footer>
+                {promotion.comments.length > 0 && (
+                    <div>"{promotion.comments[0].comment}"</div>
+                )}
 
-}
+                <div>{promotion.comments.length} Comentários</div>
+                <a href={promotion.url} target="_blank" rel="noreferrer"> Ir para o site </a>
+            </footer>
+        </div>
+    </div>)
 
-export default PromoCard
+
+export default PromotionCard
